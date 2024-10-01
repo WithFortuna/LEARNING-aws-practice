05ff229414f788db39d1a2aebb54391aaedb0c83
WithFortuna <rmsghchl0@naver.com>
2024-10-02 01:07:52 +0900

    <커밋 내용>
    1.travis.yml 파일 수정.
    :프로젝트 파일 중 jar,deploy.sh,appspec 파일만 s3에 전송
    2.appspec.yml파일 생성
    :deploy.sh 파일 자동실행하도록 설정(자동배포)
    
    3.deploy.sh 추가

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -6,10 +6,10 @@ REPOSITORY=/root/app/step2
 PROJECT_NAME=awsPractice
 
 echo "> Build 파일 복사"
-sudo chmod 777 $REPOSITORY/
-sudo chmod 777 $REPOSITORY
-sudo chmod 777 $REPOSITORY/zip
-sudo chmod 777 /root/app/application-oauth.properties /root/app/application-real-db.properties
+sudo chmod -R 777 $REPOSITORY/
+sudo chmod -R 777 $REPOSITORY
+sudo chmod -R 777 $REPOSITORY/zip
+sudo chmod -R 777 /root/app/application-oauth.properties /root/app/application-real-db.properties
 
 sudo cp $REPOSITORY/zip/*.jar $REPOSITORY/
 
